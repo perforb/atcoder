10260b05198acc38514dca9fe7c598cf8ac169b0
perforb <dev.perfumed.garden@gmail.com>
2020-07-29 23:15:26 +0900

    Update comments

--- src/jp/atcoder/ABC049C/Main.java
+++ src/jp/atcoder/ABC049C/Main.java
@@ -5,21 +5,19 @@ import java.util.Scanner;
 
 /**
  * @see "https://atcoder.jp/contests/abs/tasks/arc065_a"
- * @see "https://atcoder.jp/contests/abs/submissions/15408375"
- * @see "https://atcoder.jp/contests/abs/submissions/12814555"
  */
 public class Main {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String S = sc.next();
-    // 先に er で始まる単語を消す必要がある.
+    // 与えられた文字列を特定の順序に従って単語を消した結果, 空文字列であれば一致したものと見なす.
     // 例えば dreameraserdreamerase の場合に dreamer を先に消すと aserase となってしまう.
     // また, erase を先に消すと dreamrdream となってしまう.
     // そのため, eraser, erase, dreamer, dream の順に消す必要がある.
     var words = List.of("eraser", "erase", "dreamer", "dream");
     for (String word : words) {
-      S = S.replace(word, ""); // 一致した単語がすべて消える
+      S = S.replace(word, ""); // 一致した単語をすべて空文字列に置換
     }
     System.out.println(S.isEmpty() ? "YES" : "NO");
   }
