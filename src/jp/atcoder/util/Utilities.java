004c46def9d58f92d61f781afbeb658049b38c12
perforb <dev.perfumed.garden@gmail.com>
2020-08-03 00:47:15 +0900

    Fix access modifiers

--- src/jp/atcoder/util/Utilities.java
+++ src/jp/atcoder/util/Utilities.java
@@ -2,7 +2,7 @@ package jp.atcoder.util;
 
 public class Utilities {
 
-  private static long gcd(long a, long b) {
+  public static long gcd(long a, long b) {
     if (a < b) {
       long tmp = a;
       a = b;
@@ -14,7 +14,7 @@ public class Utilities {
     return gcd(b, a % b);
   }
 
-  private static long lcm(long a, long b) {
+  public static long lcm(long a, long b) {
     return a * b / gcd(b, a % b);
   }
 }
