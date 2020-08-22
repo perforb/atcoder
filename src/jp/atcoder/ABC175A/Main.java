b2114e642b06d27d9571f803ca814885a1865d33
perforb <dev.perfumed.garden@gmail.com>
2020-08-22 21:28:02 +0900

    Add ABC175A

---
+++ src/jp/atcoder/ABC175A/Main.java
@@ -0,0 +1,19 @@
+package jp.atcoder.ABC175A;
+
+import java.util.Scanner;
+
+/**
+ * @see "https://atcoder.jp/contests/abc175/tasks/abc175_a"
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    var s = sc.next();
+    var answer = 0;
+    for (String w : s.split("S")) {
+      answer = Math.max(answer, w.length());
+    }
+    System.out.println(answer);
+  }
+}
